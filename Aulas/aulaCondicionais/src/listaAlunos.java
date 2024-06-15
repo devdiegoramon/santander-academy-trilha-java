2906eb38f13ec840b858e4d336947c641b08127c
Diego Ramon <brilhocelestebc15@gmail.com>
2024-06-15 20:12:04 -0300

    ADD: Semana1

---
+++ Aulas/aulaCondicionais/src/listaAlunos.java
@@ -0,0 +1,9 @@
+public class listaAlunos {
+    public static void main (String[] args) {
+        String alunos [] = {"Witoria", "Diego", "João", "Josane", "Kleyton", "Redi"};
+
+        for (int x=0; x< alunos.length; x++){
+            System.out.println ("o aluno no indice x" + x + " é " + alunos[x]);
+        }
+    }
+}
