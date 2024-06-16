703af43e29560c86c95f664371024664ec3d1152
Diego Ramon <brilhocelestebc15@gmail.com>
2024-06-15 21:18:54 -0300

    CREATE/ADD: Desafi Simulação bancária, adicionado o array de opções e a impressão na tela

---
+++ bancoDigital/src/simulacaoBancaria.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+
+// ENTRADA
+
+//        O programa exibirá um menu com as seguintes opções:
+        Scanner scanner = new Scanner (System.in);
+        double saldo = 0;
+        boolean continuar = true;
+
+        while (continuar) {
+
+            int opcao = scanner.nextInt();
+
+
+            //        Depositar
+//        Sacar
+//        Consultar Saldo
+//        Encerrar
+//
+//        O usuário escolherá uma dessas opções digitando o número correspondente.
+
+// SAIDA
+
+//        Utilizando um switch, o programa realizará a operação escolhida pelo usuário.
+//
+//        Se o usuário escolher:
+//        1: O programa solicitará o valor a ser depositado e atualizará o saldo. Além disso, deve imprimir o saldo atual com a mensagem "Saldo atual: {saldo com 1 casa decimal}".
+//        2: O programa solicitará o valor a ser sacado e verificará se há saldo suficiente. Sendo assim, deve imprimir "Saldo atual: {saldo com 1 casa decimal}" quando o saldo for suficiente e "Saldo insuficiente."
+//        3: O programa apenas exibirá o saldo atual da conta: "Saldo atual: {saldo com 1 casa decimal}".
+//        0: O programa encerrará, imprimindo "Programa encerrado.".
+
+
+    }
+}
+
