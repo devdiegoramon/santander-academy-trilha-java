f075ded4f0a4696e13533b472e8b6f8476dfcfe4
Diego Ramon <brilhocelestebc15@gmail.com>
2024-07-01 02:57:05 -0300

    ADD: desafio de Abstracao Bootcamp: adicionando dados primitivos, gts and setters

---
+++ desafioAbstracaoBootcamp/src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descriçao Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Descriçao Curso Python");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria de java");
+        mentoria1.setDescricao("descrição mentoria de java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
+    }
+}
